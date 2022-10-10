3d9e76e7745cbc498a1912ca5e6815354214c205
Hayath <hayathms@protonmail.com>
2022-10-10 12:27:25 +0530

    development ...

--- start.sh
+++ start.sh
@@ -18,14 +18,29 @@ else
     echo "Continer not running";
 fi
 
-DATABASE=$(docker ps| grep $DATABASE_NAME)
-
-if [ ${#DATABASE} -ge 5 ]; then
-    echo "Database Exists";
+if [ ${#DATABASE_NAME} -ge 5 ]; then
+    DATABASE=$(docker ps| grep $DATABASE_NAME)
+    if [ ${#DATABASE} -ge 5 ]; then
+        echo "Database Exists";
+    else
+        echo "First run Database container server name '${DATABASE_NAME}'";
+        exit 1
+    fi
 else
-    echo "Run Database container server name '${DATABASE_NAME}' and docker Network '${NETWORK_NAME}' first";
-    docker network create ${NETWORK_NAME};
-    exit 1
+    echo "Database vairable not provided";
+fi
+
+if [ ${#NETWORK_NAME} -ge 5 ]; then
+    NETWORK=$(docker network ls| grep $NETWORK_NAME)
+    if [ ${#NETWORK} -ge 5 ]; then
+        NETWORK_NAME="--network '${NETWORK_NAME}'"
+        echo "Network Exists";
+    else
+        echo "Given Network Does not exits, creating one";
+        docker network create ${NETWORK_NAME};
+    fi
+else
+    echo "NETWORK_NAME vairable not provided";
 fi
 
 IMAGE=$(docker images| grep $SERVICE_IMAGE)
@@ -38,7 +53,7 @@ else
 fi
 
 #docker run --user "$(id -u):$(id -g)" -it --network bluebasket-net --name store_service -p 8001:8000 -v "/home/hayathms/GitWorld/":"/home/${USER}/GitWorld" storeservice:latest /bin/bash;
-docker run --hostname $SERVICE_NAME --user "$(id -u):$(id -g)" -it --network bluebasket-net --name $SERVICE_NAME $PORT_ADDRESS $ADDITIONAL_VOLUMES -v ${PROJECT_PWD}/../:${PROJECT_PWD}/../ "${SERVICE_IMAGE}:latest" /bin/bash;
+docker run --hostname $SERVICE_NAME --user "$(id -u):$(id -g)" -it $NETWORK_NAME --name $SERVICE_NAME $PORT_ADDRESS $ADDITIONAL_VOLUMES -v ${PROJECT_PWD}/../:${PROJECT_PWD}/../ "${SERVICE_IMAGE}:latest" /bin/bash;
 
 TAG_NUMBER=$(docker ps -a|grep $SERVICE_NAME|awk '{ print $1}');
 docker commit $TAG_NUMBER $SERVICE_IMAGE:latest;
